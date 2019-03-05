df09bcd35ed1b04518af86454c5109a9121ae6ee
Serghei Iakovlev <serghei@phalconphp.com>
2019-03-05 22:57:32 +0200

    Tune up init.el

--- init.el
+++ init.el
@@ -1,36 +1,64 @@
 ;;; init.el -- Initialization file
-;; Serghei Iakovlev
-;; 4 March 2019
+
+;; Copyright (C) 2019 Serghei Iakovlev
+
+;; Author: Serghei Iakovlev <sadhooklay@gmail.com>
+;; Version: 0.1.0
+;; URL: https://github.com/sergeyklay/emacs.d
 
 ;;; Commentary:
+
 ;; This file is used to set up the packages sources and then call
 ;; an org-babel tangle in order to load a literate configuration.
 
 ;;; Code:
 
+;; Turn off mouse interface early in startup to avoid momentary display
+(when window-system
+  (menu-bar-mode -1)
+  (tool-bar-mode -1)
+  (scroll-bar-mode -1)
+  (tooltip-mode -1))
+
+;; Startup server always
 (require 'server)
 (unless (server-running-p)
        (server-start))
 
+;; Startup screen
+(setq inhibit-startup-message t)
+(setq initial-scratch-message "")
+
+;; Set up package
 (require 'package)
+(setq package-archives
+      '(("org" . "http://orgmode.org/elpa/")
+        ("melpa" . "http://melpa.org/packages/")
+        ("melpa-stable" . "http://stable.melpa.org/packages/")
+        ("gnu" . "https://elpa.gnu.org/packages/")))
 
-(setq package-archives '(("org" . "http://orgmode.org/elpa/")
-                         ("melpa" . "http://melpa.org/packages/")
-                         ("melpa-stable" . "http://stable.melpa.org/packages/")
-                         ("gnu" . "https://elpa.gnu.org/packages/")))
+(when (boundp 'package-pinned-packages)
+  (setq package-pinned-packages
+	'((org-plus-contrib . "org"))))
 
-(setq package-enable-at-startup nil)
 (package-initialize)
 
-(defconst user-init-dir
-  (cond ((boundp 'user-emacs-directory)
-         user-emacs-directory)
-        ((boundp 'user-init-directory)
-         user-init-directory)
-        (t "~/.emacs.d/")))
+;; Bootstrap use-package
+;; Install use-package if it's not already installed.
+;; use-package is used to configure the rest of the packages.
+(unless (or (package-installed-p 'use-package)
+            (package-installed-p 'diminish))
+  (package-refresh-contents)
+  (package-install 'use-package)
+  (package-install 'diminish))
 
-; See: https://emacs.stackexchange.com/a/3147/16592
+;; From use-package README
+(eval-when-compile (require 'use-package))
+(require 'diminish)
+(require 'bind-key)
+
+;; See: https://emacs.stackexchange.com/a/3147/16592
 (org-babel-load-file
- (expand-file-name "settings.org" user-init-dir))
+ (expand-file-name "settings.org" user-emacs-directory))
 
 ;;; init.el ends here
