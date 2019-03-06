7f4f995a837970240d9373b0086b952782ebb911
Serghei Iakovlev <serghei@phalconphp.com>
2019-03-06 19:41:44 +0200

    Added treemacs and php

--- custom.el
+++ custom.el
@@ -6,7 +6,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (company yaml-mode json-mode crux undo-tree quickrun slime shell-pop scratch emmet-mode smooth-scrolling smartscan multiple-cursors markdown-mode macrostep git-link flycheck expand-region edit-server magit counsel-projectile counsel ivy helm-swoop helm-ag helm-projectile c-eldoc ace-window ace-jump-mode ag transpose-frame org-tempo ox-pandoc ob-restclient org-plus-contrib page-break-lines use-package haskell-mode doom-themes diminish))))
+    (php-mode treemacs-magit treemacs-icons-dired treemacs-projectile treemacs company yaml-mode json-mode crux undo-tree quickrun slime shell-pop scratch emmet-mode smooth-scrolling smartscan multiple-cursors markdown-mode macrostep git-link flycheck expand-region edit-server magit counsel-projectile counsel ivy helm-swoop helm-ag helm-projectile c-eldoc ace-window ace-jump-mode ag transpose-frame org-tempo ox-pandoc ob-restclient org-plus-contrib page-break-lines use-package haskell-mode doom-themes diminish))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
