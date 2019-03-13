fa78d6dd34737c6b0f50e073adb8039b77be5c15
Serghei Iakovlev <serghei@phalconphp.com>
2019-03-13 21:00:15 +0200

    Remove no longer used packages

--- .local/etc/custom.el
+++ .local/etc/custom.el
@@ -6,7 +6,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (helm-flyspell helm-words helm-flx helm-org-rifle helm-mode-manager helm-flycheck helm-descbinds editorconfig ac-php powershell intero flycheck-haskell company-ghci company-ghc hindent web-mode winum company-php php-mode company yaml-mode json-mode crux undo-tree quickrun slime shell-pop scratch emmet-mode smooth-scrolling smartscan multiple-cursors markdown-mode macrostep git-link flycheck expand-region edit-server magit counsel-projectile counsel ivy helm-swoop helm-ag helm-projectile c-eldoc ace-jump-mode ag transpose-frame org-tempo ox-pandoc ob-restclient org-plus-contrib page-break-lines use-package haskell-mode doom-themes diminish))))
+    (helm-flyspell helm-flx helm-org-rifle helm-mode-manager helm-flycheck helm-descbinds editorconfig ac-php powershell intero flycheck-haskell company-ghci company-ghc hindent web-mode winum company-php php-mode company yaml-mode json-mode crux undo-tree quickrun slime shell-pop scratch emmet-mode smooth-scrolling smartscan multiple-cursors markdown-mode macrostep git-link flycheck expand-region edit-server magit counsel ivy helm-swoop helm-ag helm-projectile c-eldoc ace-jump-mode ag transpose-frame org-tempo ox-pandoc ob-restclient org-plus-contrib page-break-lines use-package haskell-mode doom-themes diminish))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
