a09597432f12e55221333e3c158083de478b58c6
Serghei Iakovlev <serghei@phalconphp.com>
2019-04-04 20:05:07 +0300

    Reconfigure c-mode

--- features/lang/cc-lang.el
+++ features/lang/cc-lang.el
@@ -15,12 +15,28 @@
 
 ;;; Code:
 
-;;; Cmake
+;;; Packages
 
 (use-package cmake-mode
   :mode ("CMakeLists\\.txt\\'" "\\.cmake\\'"))
 
-(defun cc--common-hook ()
+(use-package c-eldoc
+  :defer t
+  :init
+  (add-hook 'c-mode-common-hook #'my--cc-eldec-hook t))
+
+(use-package company-c-headers
+  :after company
+  :hook
+  ((c-mode c++mode) . my--cc-headers-hook))
+
+;;; Hooks
+
+(add-hook 'c-mode-hook #'cc--common-hook)
+
+;;; Defuns
+
+(defun my--cc-common-hook ()
   "Change the default way that Emacs handles indentation."
   (validate-setq tab-width 4
                  c-basic-offset 4
@@ -29,17 +45,15 @@
   (c-set-offset 'substatement-open 0)
   (c-set-offset 'case-label 4))
 
-(add-hook 'c-mode-hook #'cc--common-hook)
+(defun my--cc-eldec-hook ()
+  "ElDoc setup."
+  (set (make-local-variable 'eldoc-idle-delay) 0.20)
+  (c-turn-on-eldoc-mode))
 
-(defun cc--headers-hook ()
+(defun my--cc-headers-hook ()
   "Enable `company-mode' and add `company-c-headers' to the `company-backends'."
   (make-local-variable 'company-backends)
   (add-to-list 'company-backends 'company-c-headers))
-
-(use-package company-c-headers
-  :after company
-  :hook
-  ((c-mode c++mode) . cc--headers-hook))
 
 (provide 'cc-lang)
 ;;; cc-lang.el ends here
