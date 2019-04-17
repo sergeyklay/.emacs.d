446a0ec2ac59e877323fd82bfead6e3371dca50d
Serghei Iakovlev <serghei@phalconphp.com>
2019-04-17 21:41:00 +0300

    Move personal settings to custom.el

--- init.el
+++ init.el
@@ -26,7 +26,6 @@
 (require 'bookmarks)    ; Setting for bookmarks, recentf, etc
 (require 'docs)         ; Setting up documentation features
 (require 'appearance)   ; Appearance related settings
-(require 'user)         ; Personalization
 (require 'secure)       ; Security related features
 (require 'windows)      ; Windows management features
 (require 'modeline)     ; Modeline related configuration
