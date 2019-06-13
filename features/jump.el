1d98c1d850eb19019d9112c779ce778ee0bb95f5
Serghei Iakovlev <sadhooklay@gmail.com>
2019-06-13 12:51:46 +0300

    Fixed ggtags key bindigs

--- features/jump.el
+++ features/jump.el
@@ -18,26 +18,26 @@
 (defconst global-executable-path (executable-find "global")
   "The global executable path on this system.")
 
-(defconst gtags-enable-by-default t
-  "Whether or not to enable ggtags-mode.")
-
 ;; A front-end for accessing the gtags-generated tags.
 ;; For more see URL `https://github.com/leoliu/ggtags'
 (use-package ggtags
-  :if (and global-executable-path gtags-enable-by-default)
-  :bind
-  (:map ggtags-mode-map
-        ("C-c g s" . 'ggtags-find-other-symbol)
-        ("C-c g h" . 'ggtags-view-tag-history)
-        ("C-c g r" . 'ggtags-find-reference)
-        ("C-c g f" . 'ggtags-find-file)
-        ("C-c g c" . 'ggtags-create-tags)
-        ("C-c g u" . 'ggtags-update-tags)
-        ("M-."     . 'ggtags-find-tag-dwim)
-        ("M-,"     . 'pop-tag-mark)
-        ("M-]"     . nil)
-        ("C-c <"   . 'ggtags-prev-mark)
-        ("C-c >"   . 'ggtags-next-mark)))
+  :if global-executable-path
+  :config
+  (unbind-key "M-]" ggtags-mode-map)
+  (unbind-key "M-o" ggtags-navigation-map)
+  :bind (:map ggtags-mode-map
+	 ("C-c g s" . 'ggtags-find-other-symbol)
+	 ("C-c g h" . 'ggtags-view-tag-history)
+	 ("C-c g r" . 'ggtags-find-reference)
+	 ("C-c g f" . 'ggtags-find-file)
+	 ("C-c g c" . 'ggtags-create-tags)
+	 ("C-c g u" . 'ggtags-update-tags)
+	 ("M-."     . 'ggtags-find-tag-dwim)
+	 ("M-,"     . 'pop-tag-mark)
+	 ("C-c <"   . 'ggtags-prev-mark)
+	 ("C-c >"   . 'ggtags-next-mark)
+	 :map ggtags-navigation-map
+	 ("M-l"     . 'ggtags-navigation-visible-mode)))
 
 ;; Make Emacs reload the TAGS file automatically
 (setq tags-revert-without-query 1)
@@ -53,7 +53,7 @@
 
 For eldoc, ggtags advises the eldoc function at the lowest priority
 so that if the major mode has better support it will use it first."
-  (when (and global-executable-path gtags-enable-by-default)
+  (when global-executable-path
     (ggtags-mode 1)
     (eldoc-mode 1)))
 
