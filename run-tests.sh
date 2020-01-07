681b24aefd73c63d9d737a179be6c4b17a36d0c3
Serghei Iakovlev <sadhooklay@gmail.com>
2020-01-07 18:03:04 +0200

    Correct test runner

--- run-tests.sh
+++ run-tests.sh
@@ -2,15 +2,13 @@
 
 set -eu
 
-PROJECT_ROOT=$(cd "$(dirname $0)/../"; pwd)
-ln -s $PROJECT_ROOT $HOME/.emacs.d
-
 ${EMACS:=emacs} \
     -nw \
+    -u $(pwd)/init.el \
     --batch \
     --eval '(let ((debug-on-error t)
-    	          (url-show-status nil)
-		  (user-emacs-directory default-directory)
-		  (user-init-file (expand-file-name "init.el"))
-		  (load-path (delq default-directory load-path)))
-	       (load-file user-init-file))'
+                  (url-show-status nil)
+                  (user-emacs-directory default-directory)
+                  (user-init-file (expand-file-name "init.el"))
+                  (load-path (delq default-directory load-path)))
+               (load-file user-init-file))'
