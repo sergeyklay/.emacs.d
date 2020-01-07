1fb0eda4075f1f803e67ceace4a6bff341d80060
Serghei Iakovlev <sadhooklay@gmail.com>
2020-01-07 18:52:17 +0200

    Correct test runner II

--- run-tests.sh
+++ run-tests.sh
@@ -1,8 +1,25 @@
-#!/bin/sh
+#!/bin/sh -xe
 
-set -eu
+echo "Attempting startup..."
 
-EMACS="${EMACS:=emacs}"
+( echo "$EMACS" | grep -q "term" ) && EMACS=emacs || EMACS=${EMACS:-emacs}
+
+command -v $EMACS >/dev/null || {
+  >&2 echo "Can't find GNU Emacs in your PATH"
+  exit 1
+}
+
+case "$VERSION" in *\ 2[0-5].[0-9])
+  echo "Detected Emacs $VERSION"
+  echo "This configuration only supports Emacs 26.0 and newer"
+  exit 2
+  ;;
+esac
+
+[ "$1" = -d ] || [ "$1" = --debug ] && {
+    shift
+    export DEBUG=1
+}
 
 ${EMACS} \
     --no-window-system \
@@ -12,4 +29,7 @@ ${EMACS} \
                   (user-emacs-directory default-directory)
                   (user-init-file (expand-file-name "init.el"))
                   (load-path (delq default-directory load-path)))
-               (load-file user-init-file))'
+               (load-file user-init-file)
+               (run-hooks (quote after-init-hook)))'
+
+echo "Startup successful"
