48920b876bfe108f3945aa3f95b8181640d41b2e
Serghei Iakovlev <sadhooklay@gmail.com>
2020-01-07 19:26:07 +0200

    Cleaned up test runner

--- run-tests.sh
+++ run-tests.sh
@@ -1,20 +1,13 @@
-#!/bin/sh -xe
+#!/bin/sh -eu
 
 echo "Attempting startup..."
 
-( echo "$EMACS" | grep -q "term" ) && EMACS=emacs || EMACS=${EMACS:-emacs}
+EMACS=${EMACS:-emacs}
 
 command -v $EMACS >/dev/null || {
   >&2 echo "Can't find GNU Emacs in your PATH"
   exit 1
 }
-
-case "$VERSION" in *\ 2[0-5].[0-9])
-  echo "Detected Emacs $VERSION"
-  echo "This configuration only supports Emacs 26.0 and newer"
-  exit 2
-  ;;
-esac
 
 [ "$1" = -d ] || [ "$1" = --debug ] && {
     shift
