0d1a5b69aef254ef2cb8f6f65ad5927181f4dd2b
Serghei Iakovlev <sadhooklay@gmail.com>
2020-01-09 11:46:48 +0200

    Added personalization and prelude configuration

--- init.el
+++ init.el
@@ -17,54 +17,14 @@
 
 ;;; Code:
 
-;;; Begin initialization
-
-;; Turn off mouse interface early in startup to avoid momentary display.
-(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))     ; Disable the menu bar
-(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))     ; Disable the tool bar
-(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1)) ; Disable the scroll bar
-(if (fboundp 'tooltip-mode) (tooltip-mode -1))       ; Disable the tooltips
-
-;; Don't save customizations at all, prefer to set things explicitly.
-(setq custom-file null-device)
-
-;; One less file to load at startup.
-(setq site-run-file nil)
-
-;; Disable start-up screen.
-(setq inhibit-startup-screen t)
-
-;; Actually this project is my personal configuration
-;; so I use GNU Emacs 26.1 now.
-(eval-when-compile
-  (and (version< emacs-version "26.1")
-       (error
-        (concat
-         "Detected Emacs %s. "
-         "This configuration is designed to work "
-         "only with Emacs 26.1 and higher. I'm sorry.")
-        emacs-version)))
-
-(defconst emacs-debug-mode (or (getenv "DEBUG") init-file-debug)
-  "If non-nil, all Emacs will be verbose.
-Set DEBUG=1 in the command line or use --debug-init to enable this.")
-
-(require 'directories (concat user-emacs-directory "settings/directories"))
-
-;; Setting up the dependencies, features and packages
-(add-to-list 'load-path user-host-dir)
-(add-to-list 'load-path user-site-lisp-dir)
-(add-to-list 'load-path user-settings-dir)
-
-;;; Personal information
-
-(setq user-full-name "Serghei Iakovlev"
-      user-mail-address "egrep@protonmail.ch")
+;; Begin initialization
+(require 'directories (concat user-emacs-directory "settings/prelude"))
 
 (require 'packaging)  ; Package management stuff and various related settings
 (require 'appearance) ; Set up appearance as soon as we can
-(require 'backup)
+(require 'backup)     ; Backup, auto-saving and history configuration
 (require 'editor)     ; Features related to the behavior of the editor
+(require 'p13n)       ; Personalization related stuff
 (require 'vcs)        ; VCS (mostly git) related features
 
 ;;; Languages Support
---
+++ settings/p13n.el
@@ -0,0 +1,20 @@
+;;; p13n.el --- Personalization settings. -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2019-2020 Serghei Iakovlev <egrep@protonmail.ch>
+
+;; Author: Serghei Iakovlev <egrep@protonmail.ch>
+;; URL: https://github.com/sergeyklay/.emacs.d
+;;
+;; This file is NOT part of GNU Emacs.
+;;
+;; License: GPLv3
+
+;;; Commentary:
+
+;; This file should contain all personalization related stuff.
+
+(setq user-full-name "Serghei Iakovlev"
+      user-mail-address "egrep@protonmail.ch")
+
+(provide 'p13n)
+;;; p13n.el ends here
