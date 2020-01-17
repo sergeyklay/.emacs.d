4962f72ad0b047d196369e3f36e005edbb875ea9
Serghei Iakovlev <sadhooklay@gmail.com>
2020-01-17 23:31:24 +0200

    Correct eshell-git-prompt-use-theme call

--- site-lisp/shells.el
+++ site-lisp/shells.el
@@ -64,7 +64,7 @@
 
   (use-package eshell-git-prompt
     :init
-    (eshell-git-prompt-use-theme 'radar)))
+    (eshell-git-prompt-use-theme 'git-radar)))
 
 (with-eval-after-load 'eshell-mode
   (define-key eshell-mode-map (kbd "C-a") 'eshell-bol))
