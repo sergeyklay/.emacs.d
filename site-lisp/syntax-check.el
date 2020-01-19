b5b8de4f7dfec7f9d9fe2fac73b25a959613567d
Serghei Iakovlev <egrep@protonmail.ch>
2020-01-19 15:54:43 +0200

    Rework flycheck configuration

--- site-lisp/syntax-check.el
+++ site-lisp/syntax-check.el
@@ -15,27 +15,54 @@
 
 ;;; Code:
 
-;;;; Flycheckf
-
+;;;; Flycheck
 
 ;; Flycheck is a general syntax highlighting framework which
 ;; other packages hook into.  It's an improvment on the built
 ;; in flymake.
-
 (use-package flycheck
+  :if (not (equal system-type 'windows-nt))
+  :diminish flycheck-mode
   :preface
-  ;; See URL `https://github.com/flycheck/flycheck/issues/302'
-  (defun unless-error-buffer (errors)
-    (unless (get-buffer-window flycheck-error-list-buffer)
-      (when (fboundp 'flycheck-display-error-messages)
-	(flycheck-display-error-messages errors))))
+  (defun my/adjust-flycheck-automatic-syntax-eagerness ()
+    "Adjust how often we check for errors based on if there are any.
 
-  (global-flycheck-mode 1)
+This lets us fix any errors as quickly as possible, but in a clean
+buffer we're an order of magnitude laxer about checking."
+    (setq flycheck-idle-change-delay
+	  (if flycheck-current-errors 0.5 30.0)))
+  :custom
+  (flycheck-indication-mode 'right-fringe)
+  (flycheck-standard-error-navigation nil)
+  (flycheck-emacs-lisp-load-path 'inherit)
+  ;; Remove ‘new-line’ checks, since they would trigger an immediate
+  ;; check when we want the idle-change-delay to be effect while
+  ;; editing.
+  (flycheck-check-syntax-automatically
+   '(save idle-change mode-enabled))
+  :hook
+  ((flycheck-after-sytax-check . my/flycheck-adjust-syntax-eagerness))
   :config
-  (setq flycheck-indication-mode 'right-fringe
-	flycheck-standard-error-navigation nil
-	flycheck-display-errors-function #'unless-error-buffer
-	flycheck-emacs-lisp-load-path 'inherit))
+  (progn
+    ;; Each buffer gets its own idle-change-delay because of the
+    ;; buffer-sensitive adjustment above.
+    (make-variable-buffer-local 'flycheck-idle-change-delay)
+    (global-flycheck-mode)))
+
+;;;; flycheck-tip
+
+(use-package flycheck-tip
+  :after flycheck
+  :custom (flycheck-tip-avoid-show-func nil))
+
+;;;; flycheck-pos-tip
+
+(use-package flycheck-pos-tip
+  :defer t
+  :if (display-graphic-p)
+  :after flycheck
+  :commands flycheck-pos-tip-mode
+  :hook (flycheck-mode . flycheck-pos-tip-mode))
 
 (provide 'syntax-check)
 ;;; syntax-check.el ends here
