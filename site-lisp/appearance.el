912267376e0f8f4aee639224ad403b4384bf3ba1
Serghei Iakovlev <egrep@protonmail.ch>
2020-01-26 03:19:25 +0200

    Add theme related stuff

--- site-lisp/appearance.el
+++ site-lisp/appearance.el
@@ -56,13 +56,51 @@
 	(my/fonts-darwin)))
       (add-to-list 'face-font-rescale-alist '(".*icons.*" . 0.9))
       (add-to-list 'face-font-rescale-alist '(".*FontAwesome.*" . 0.9))))
+
+  ;; I tend to switch themes more often than normal.  For example,
+  ;; switching to a lighter theme (such as the default) or to a
+  ;; different theme depending on the time of day or my mood.  Normally,
+  ;; switching themes is a multi-step process with `disable-theme' and
+  ;; `load-theme'.  The `my/select-theme' function will do that in one
+  ;; swoop.  I just choose which theme I want to go to.
+
+  (defun my/enable-theme (theme)
+    "Disable any currently active themes and load the THEME."
+    (let ((enabled-themes custom-enabled-themes))
+      (mapc #'disable-theme enabled-themes)
+      (load-theme theme t)))
+
+  (defun my/default-theme ()
+    "Load the default theme."
+    (my/enable-theme 'leuven))
+
+  (defun my/toggle-theme ()
+    "Simplistic toggle for my used themes.
+All it does is check if `leuven' (light version) is active and if so switch to
+`wombat' (dark version).  Else it switches to the light theme."
+    (interactive)
+    (if (eq (car custom-enabled-themes) 'leuven)
+	(my/enable-theme 'wombat)
+      (my/enable-theme 'leuven)))
+
+  (defun my/select-theme (theme)
+    "Provide a way to select and enable custom THEME."
+    ;; This interactive call is taken from `load-theme'
+    (interactive
+     (list
+      (intern (completing-read "Load custom theme: "
+			       (mapc 'symbol-name
+				     (custom-available-themes))))))
+    (my/enable-theme theme))
   :custom
   (x-underline-at-descent-line t)
   (underline-minimum-offset 1)
   ;; Don't beep at me.
   (visible-bell t)
-  :hook (after-init . my/fonts-setup))
-
+  :bind ([C-f11] . #'my/select-theme)
+  :bind ([C-f12] . #'my/toggle-theme)
+  :hook ((after-init . my/fonts-setup )
+	 (after-init . my/default-theme)))
 
 ;; Throw away the mouse when typing.
 ;; Move the mouse to the corner only if the cursor gets too close,
@@ -83,9 +121,6 @@
 
 ;; Highlight matching parentheses when the point is on them.
 (show-paren-mode 1)
-
-;; I like light themes. Use `leuven' by default.
-(load-theme 'leuven t)
 
 (provide 'appearance)
 ;;; appearance.el ends here
