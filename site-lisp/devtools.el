68aebc9ccd0ad5c2313e7262cb75b9f4b3d7d345
Serghei Iakovlev <egrep@protonmail.ch>
2020-02-09 23:09:44 +0200

    Measure the current start up time

--- site-lisp/devtools.el
+++ site-lisp/devtools.el
@@ -20,5 +20,14 @@
 (use-package esup
   :commands (esup))
 
+;; Measure the current start up time.
+(add-hook 'emacs-startup-hook
+          (lambda ()
+            (message "Emacs ready in %s with %d garbage collections."
+                     (format "%.2f seconds"
+                             (float-time
+                              (time-subtract after-init-time before-init-time)))
+                     gcs-done)))
+
 (provide 'devtools)
 ;;; devtools.el ends here
