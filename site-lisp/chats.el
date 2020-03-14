be618645b9ece21b970523becc396fb51559b1b1
Serghei Iakovlev <egrep@protonmail.ch>
2020-03-14 19:44:56 +0200

    Reconfigure erc

--- site-lisp/chats.el
+++ site-lisp/chats.el
@@ -15,12 +15,16 @@
 
 ;;; Code:
 
-(require 'directories)
+(eval-when-compile
+  (require 'directories))
+
+(defconst erc-logging-directory
+  (concat user-local-dir "logs/erc/"))
 
 (defun erc-logging-hook ()
   "Setting up channel logging for `erc'."
   (eval-when-compile (require 'erc-log nil t))
-  (let ((log-channels-directory (concat user-local-dir "logs/erc/")))
+  (let ((log-channels-directory erc-logging-directory))
     (setq erc-log-channels-directory log-channels-directory
 	  erc-log-insert-log-on-open t)
     (unless (file-exists-p log-channels-directory)
@@ -32,27 +36,32 @@
   :custom
   (erc-autojoin-channels-alist
    '(("freenode.net"
-      "#emacs" "#latex" "#org-mode"
-      "#clojure" "#phalcon" "#zephir")))
+      "#emacs"
+      ;; "#latex"
+      ;; "#org-mode"
+      "#clojure"
+      "#phalcon"
+      "#zephir")))
+  (erc-try-new-nick-p t)
+  (erc-nick-uniquifier "_")
+  (erc-user-full-name user-full-name)
   (erc-autojoin-timing 'ident)
+  (erc-server-coding-system '(utf-8 . utf-8))
   (erc-fill-function 'erc-fill-static)
   (erc-fill-static-center 22)
   (erc-hide-list '("JOIN" "PART" "QUIT"))
+  (erc-track-exclude-types '("JOIN" "MODE" "NICK" "PART" "QUIT"
+                             "324" "329" "332" "333" "353" "477"))
   (erc-lurker-hide-list '("JOIN" "PART" "QUIT"))
   (erc-lurker-threshold-time 43200)
   ;; For more see
   ;; URL `https://www.gnu.org/software/emacs/manual/html_node/erc/Connecting.html'
   (erc-prompt-for-nickserv-password nil)
-  (erc-server-reconnect-attempts 5)
+  (erc-server-reconnect-attempts 6)
   (erc-server-reconnect-timeout 3)
-  (erc-track-exclude-types '("JOIN" "MODE" "NICK" "PART" "QUIT"
-                             "324" "329" "332" "333" "353" "477"))
   :config
   (dolist (module '(notifications spelling log))
     (add-to-list 'erc-modules module))
-  (erc-services-mode 1)
-  (when (fboundp 'erc-update-modules)
-    (erc-update-modules))
   :hook
   (erc-mode . erc-logging-hook))
 
@@ -61,14 +70,8 @@
 (use-package erc-hl-nicks
   :after erc)
 
-(use-package erc-image
-  :defer t
-  :init (with-eval-after-load 'erc
-	  (require 'erc-image)
-	  (add-to-list 'erc-modules 'image)))
-
-(declare-function erc (&key (server (erc-compute-server))))
 (declare-function erc-track-switch-buffer (arg))
+(declare-function erc-update-modules ())
 
 (defun my/erc-start-or-switch ()
   "Connects to ERC, or switch to last active buffer."
@@ -76,7 +79,15 @@
   (if (get-buffer "irc.freenode.net:6667")
       (erc-track-switch-buffer 1)
     (when (y-or-n-p "Start ERC? ")
-      (erc :server "irc.freenode.net" :port 6667))))
+      (progn
+	(erc-services-mode 1)
+	(erc-update-modules)
+	(erc :server "irc.freenode.net" :port 6667)))))
+
+(defun my/search-erc-logs (term)
+  "Search the irc logs for a given TERM."
+  (interactive "sTerm to search for in the logs: ")
+  (counsel-ag term erc-logging-directory))
 
 (provide 'chats)
 ;;; chats.el ends here
