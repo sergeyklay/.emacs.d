b1da87077815da6f119191c062619a8eac42bf82
Serghei Iakovlev <egrep@protonmail.ch>
2020-04-12 00:00:51 +0300

    Improve ecfg tools

--- defuns/emacscfg.el
+++ defuns/emacscfg.el
@@ -18,29 +18,42 @@
 (require 'cl-lib)
 (require 'projectile)
 
+(defconst ecfg-config-file ".emacscfg"
+  "The configuration file name being used to configure current project.")
+
+(defun ecfg--create-config ()
+  "Create an empty config."
+  (make-hash-table :test 'equal))
+
 (defun ecfg--read-from-file (filename)
   "Read data from FILENAME."
-  (with-temp-buffer
-    (insert-file-contents filename)
-    (cl-assert (eq (point) (point-min)))
-    (read (current-buffer))))
+  (let (data)
+    (with-temp-buffer
+      (insert-file-contents filename)
+      (cl-assert (eq (point) (point-min)))
+      (setq data (read (current-buffer)))
+      (unless (hash-table-p data)
+        (setq data (ecfg--create-config))))
+    data))
 
 (defun ecfg--print-to-file (filename data)
-  "Write DATA for FILENAME."
+  "Write a DATA to the FILENAME."
   (with-temp-file filename
     (prin1 data (current-buffer))))
 
 (defun ecfg-read-project-config ()
-  "Read project configuration."
-  (let ((path (concat (projectile-project-root) ".emacscfg")))
+  "Read the project configuration."
+  (let ((path (concat (projectile-project-root) ecfg-config-file)))
     (if (file-exists-p path)
         (ecfg--read-from-file path)
-      (make-hash-table :test 'equal))))
+      (ecfg--create-config))))
 
-(defun ecfg-save-project-config (config)
-  "Save project CONFIG."
-  (let ((path (concat (projectile-project-root) ".emacscfg")))
-    (ecfg--print-to-file path config)))
+(defun ecfg-save-project-config (data)
+  "Save the project configuration to file.
+DATA should represent a valid hashtable object."
+  (let ((path (concat (projectile-project-root) ecfg-config-file)))
+    (when (hash-table-p data)
+      (ecfg--print-to-file path data))))
 
 (provide 'emacscfg)
 ;;; emacscfg.el ends here
