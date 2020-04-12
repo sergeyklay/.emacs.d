d45a62054c03125fddf1edf5163c70452ab8acb6
Serghei Iakovlev <egrep@protonmail.ch>
2020-04-12 10:40:58 +0300

    Step 1: Implement ecfg--get-project-root

--- defuns/emacscfg.el
+++ defuns/emacscfg.el
@@ -16,10 +16,59 @@
 ;;; Code:
 
 (require 'cl-lib)
-(require 'projectile)
 
-(defconst ecfg-config-file ".emacscfg"
+(defconst ecfg-project-root-dir-use-truename t
+  "Non-nil means always expand filenames using function `file-truename'.")
+
+(defconst ecfg-config-file ".ecfg.el"
   "The configuration file name being used to configure current project.")
+
+(defun ecfg--get-project-root ()
+  "Get the project root directory of the curent opened buffer."
+  (let ((file-name (buffer-file-name))
+        (project-root (when (fboundp 'projectile-project-root)
+                        (projectile-project-root))))
+    (unless project-root
+      ;; Get the current working directory using `buffer-file-name'
+      ;; or `default-directory'
+      (if file-name
+          (setq project-root (file-name-directory file-name))
+        (setq project-root (expand-file-name default-directory)))
+
+      ;; Expand real path of the obtained working directory (if enabled)
+      (when ecfg-project-root-dir-use-truename
+        (setq project-root (file-truename project-root)))
+
+      ;; Scan for the real project root of the opened file.
+      ;; We're looking either for the `ecfg-config-file' file,
+      ;; or the '.projectile' file, or the '.git' directory.
+      (let (last-dir)
+        (while
+            (not (or (file-exists-p (concat project-root ecfg-config-file))
+                     (file-exists-p (concat project-root ".projectile"))
+                     (file-directory-p (concat project-root ".git"))
+                     (string= project-root "/")))
+          (setq last-dir project-root
+                project-root (file-name-directory
+                              (directory-file-name project-root)))
+          (when (string= last-dir project-root)
+            (setq project-root "/")))))
+
+    (when (or (string= project-root "/") (equal project-root ""))
+      (message "ECFG: Unable to resolve project root")
+      (setq project-root nil))
+
+    ;; Return resolved project root
+    project-root))
+
+(defun ecfg--init-project (project-root force)
+  "Init configuration located at PROJECT-ROOT taking into account FORCE flag."
+  )
+
+(defun ecfg-init ()
+  "Initialize ECFG configuration."
+  (interactive)
+  (ecfg--init-project (ecfg--get-project-root) t))
 
 (defun ecfg--create-config ()
   "Create an empty config."
