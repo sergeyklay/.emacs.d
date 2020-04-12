a1b276b0d9d191ef7e47898082fe3daf16cf7434
Serghei Iakovlev <egrep@protonmail.ch>
2020-04-12 11:35:14 +0300

    Step 2: Rename project-root to workspace

--- defuns/emacscfg.el
+++ defuns/emacscfg.el
@@ -17,92 +17,93 @@
 
 (require 'cl-lib)
 
-(defconst ecfg-project-root-dir-use-truename t
+(defconst ecfg-use-truename t
   "Non-nil means always expand filenames using function `file-truename'.")
 
-(defconst ecfg-config-file ".ecfg.el"
-  "The configuration file name being used to configure current project.")
+(defconst ecfg-workspace-name ".ecfg"
+  "Workspace directory name being used to configure current project.")
 
-(defun ecfg--get-project-root ()
-  "Get the project root directory of the curent opened buffer."
+(defun ecfg--workspace-root ()
+  "Resolve workspace root path based on currently opened buffer."
   (let ((file-name (buffer-file-name))
-        (project-root (when (fboundp 'projectile-project-root)
-                        (projectile-project-root))))
-    (unless project-root
+        (workspace (when (fboundp 'projectile-project-root)
+                     (projectile-project-root))))
+    (unless workspace
       ;; Get the current working directory using `buffer-file-name'
       ;; or `default-directory'
       (if file-name
-          (setq project-root (file-name-directory file-name))
-        (setq project-root (expand-file-name default-directory)))
+          (setq workspace (file-name-directory file-name))
+        (setq workspace (expand-file-name default-directory)))
 
       ;; Expand real path of the obtained working directory (if enabled)
-      (when ecfg-project-root-dir-use-truename
-        (setq project-root (file-truename project-root)))
+      (when ecfg-use-truename
+        (setq workspace (file-truename workspace)))
 
-      ;; Scan for the real project root of the opened file.
-      ;; We're looking either for the `ecfg-config-file' file,
+      ;; Scan for the real workspace path of the opened file.
+      ;; We're looking either for the `ecfg-config-dir' directiry,
       ;; or the '.projectile' file, or the '.git' directory.
       (let (last-dir)
         (while
-            (not (or (file-exists-p (concat project-root ecfg-config-file))
-                     (file-exists-p (concat project-root ".projectile"))
-                     (file-directory-p (concat project-root ".git"))
-                     (string= project-root "/")))
-          (setq last-dir project-root
-                project-root (file-name-directory
-                              (directory-file-name project-root)))
-          (when (string= last-dir project-root)
-            (setq project-root "/")))))
+            (not (or (file-directory-p (concat workspace ecfg-workspace-name))
+                     (file-exists-p (concat workspace ".projectile"))
+                     (file-directory-p (concat workspace ".git"))
+                     (string= workspace "/")))
+          (setq last-dir workspace
+                workspace (file-name-directory
+                           (directory-file-name workspace)))
+          (when (string= last-dir workspace)
+            (setq workspace "/")))))
 
-    (when (or (string= project-root "/") (equal project-root ""))
-      (message "ECFG: Unable to resolve project root")
-      (setq project-root nil))
+    (when (or (string= workspace "/") (equal workspace ""))
+      (message "ECFG: Unable to resolve workspace root path")
+      (setq workspace nil))
 
-    ;; Return resolved project root
-    project-root))
+    ;; Return resolved workspace root path
+    workspace))
 
-(defun ecfg--init-project (project-root force)
-  "Init configuration located at PROJECT-ROOT taking into account FORCE flag."
-  )
+(defun ecfg--create-workspace (project-root force)
+  "Create a workspace located at PROJECT-ROOT taking into account FORCE flag."
+  (message "workspace: %S force: %S" project-root force))
 
-(defun ecfg-init ()
-  "Initialize ECFG configuration."
+(defun ecfg-init-workspace ()
+  "Initialize ECFG workspace.
+ECFG workspace is usually just your project root folder."
   (interactive)
-  (ecfg--init-project (ecfg--get-project-root) t))
+  (ecfg--create-workspace (ecfg--workspace-root) t))
 
-(defun ecfg--create-config ()
-  "Create an empty config."
-  (make-hash-table :test 'equal))
+;; (defun ecfg--create-config ()
+;;   "Create an empty config."
+;;   (make-hash-table :test 'equal))
 
-(defun ecfg--read-from-file (filename)
-  "Read data from FILENAME."
-  (let (data)
-    (with-temp-buffer
-      (insert-file-contents filename)
-      (cl-assert (eq (point) (point-min)))
-      (setq data (read (current-buffer)))
-      (unless (hash-table-p data)
-        (setq data (ecfg--create-config))))
-    data))
+;; (defun ecfg--read-from-file (filename)
+;;   "Read data from FILENAME."
+;;   (let (data)
+;;     (with-temp-buffer
+;;       (insert-file-contents filename)
+;;       (cl-assert (eq (point) (point-min)))
+;;       (setq data (read (current-buffer)))
+;;       (unless (hash-table-p data)
+;;         (setq data (ecfg--create-config))))
+;;     data))
 
-(defun ecfg--print-to-file (filename data)
-  "Write a DATA to the FILENAME."
-  (with-temp-file filename
-    (prin1 data (current-buffer))))
+;; (defun ecfg--print-to-file (filename data)
+;;   "Write a DATA to the FILENAME."
+;;   (with-temp-file filename
+;;     (prin1 data (current-buffer))))
 
-(defun ecfg-read-project-config ()
-  "Read the project configuration."
-  (let ((path (concat (projectile-project-root) ecfg-config-file)))
-    (if (file-exists-p path)
-        (ecfg--read-from-file path)
-      (ecfg--create-config))))
+;; (defun ecfg-read-project-config ()
+;;   "Read the project configuration."
+;;   (let ((path (concat (projectile-project-root) ecfg-config-file)))
+;;     (if (file-exists-p path)
+;;         (ecfg--read-from-file path)
+;;       (ecfg--create-config))))
 
-(defun ecfg-save-project-config (data)
-  "Save the project configuration to file.
-DATA should represent a valid hashtable object."
-  (let ((path (concat (projectile-project-root) ecfg-config-file)))
-    (when (hash-table-p data)
-      (ecfg--print-to-file path data))))
+;; (defun ecfg-save-project-config (data)
+;;   "Save the project configuration to file.
+;; DATA should represent a valid hashtable object."
+;;   (let ((path (concat (projectile-project-root) ecfg-config-file)))
+;;     (when (hash-table-p data)
+;;       (ecfg--print-to-file path data))))
 
 (provide 'emacscfg)
 ;;; emacscfg.el ends here
