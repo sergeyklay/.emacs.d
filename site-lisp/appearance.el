37f22569dd1ae2295b38d6a32f4f178ee9971a64
Serghei Iakovlev <egrep@protonmail.ch>
2020-12-13 01:48:51 +0200

    Get my initial frame to be the desired size

--- site-lisp/appearance.el
+++ site-lisp/appearance.el
@@ -54,5 +54,21 @@
 ;; Highlight matching parentheses when the point is on them.
 (add-hook 'after-init-hook 'show-paren-mode)
 
+;; Get my initial frame to be the desired size
+(when window-system
+  (let (lpos tpos width-gain height-gain fwidth fheight)
+    (if (>= (x-display-pixel-width) 2000)
+        (progn (setq width-gain 0.3) (setq lpos 2000))
+      (progn (setq width-gain 0.5) (setq lpos 50)))
+    (setq tpos 50)
+    (setq height-gain 0.8)
+    (setq fwidth (floor (/ (* (x-display-pixel-width) width-gain)
+                           (frame-char-width))))
+    (setq fheight (floor (/ (* (x-display-pixel-height) height-gain)
+                            (frame-char-height))))
+
+    (set-frame-size (selected-frame) fwidth fheight)
+    (set-frame-position (selected-frame) lpos tpos)))
+
 (provide 'appearance)
 ;;; appearance.el ends here
