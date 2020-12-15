1bcb880e554b3617e0de482e31d5375d54d23813
Serghei Iakovlev <egrep@protonmail.ch>
2020-12-15 09:53:31 +0200

    Reformat show-point-mode header

--- site-lisp/show-point-mode.el
+++ site-lisp/show-point-mode.el
@@ -1,6 +1,5 @@
 ;;; show-point-mode.el --- show point position in status bar
 
-
 ;; Copyright (C) 2007, 2010, 2012 Toby Cubitt
 
 ;; Author: Toby Cubitt <toby-predictive@dr-qubit.org>
@@ -9,23 +8,24 @@
 ;; URL: http://www.dr-qubit.org/emacs.php
 
 ;; This file is NOT part of Emacs.
-;;
-;; This file is free software: you can redistribute it and/or modify it under
-;; the terms of the GNU General Public License as published by the Free
-;; Software Foundation, either version 3 of the License, or (at your option)
-;; any later version.
-;;
-;; This program is distributed in the hope that it will be useful, but WITHOUT
-;; ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-;; FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
-;; more details.
-;;
-;; You should have received a copy of the GNU General Public License along
-;; with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
 
+;;;; License
+
+;; This file is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License
+;; as published by the Free Software Foundation; either version 3
+;; of the License, or (at your option) any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this file.  If not, see <https://www.gnu.org/licenses/>.
 
 ;;; Commentary:
-;;
+
 ;; A simple minor-mode to display the point in the status bar.
 
 
