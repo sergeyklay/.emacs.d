5f6ca6a157ac8970c4711a4927fed9ef5dd5de69
Serghei Iakovlev <egrep@protonmail.ch>
2020-12-20 19:57:30 +0200

    Remove no longer used defuns dir

--- site-lisp/directories.el
+++ site-lisp/directories.el
@@ -1,13 +1,26 @@
 ;;; directories.el --- Global directories configuration. -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2019-2020 Serghei Iakovlev <egrep@protonmail.ch>
+;; Copyright (C) 2019, 2020 Serghei Iakovlev <egrep@protonmail.ch>
 
 ;; Author: Serghei Iakovlev <egrep@protonmail.ch>
 ;; URL: https://github.com/sergeyklay/.emacs.d
-;;
-;; This file is NOT part of GNU Emacs.
-;;
-;; License: GPLv3
+
+;; This file is NOT part of Emacs.
+
+;;;; License
+
+;; This file is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License
+;; as published by the Free Software Foundation; either version 3
+;; of the License, or (at your option) any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this file.  If not, see <https://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
@@ -17,9 +30,6 @@
 
 (defconst user-site-lisp-dir (concat user-emacs-directory "site-lisp/")
   "Local site-lisp directory.")
-
-(defconst user-defuns-dir (concat user-emacs-directory "defuns/")
-  "A common place to store user's defuns.")
 
 (defconst user-private-dir (concat user-emacs-directory "private/")
   "This is the default place where to store your private configuration layers.
