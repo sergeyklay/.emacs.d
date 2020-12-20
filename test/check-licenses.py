4cc76ad65990ef3d8044660ed8b8371710dd148c
Serghei Iakovlev <egrep@protonmail.ch>
2020-12-20 21:45:45 +0200

    Add missed license note

--- test/check-licenses.py
+++ test/check-licenses.py
@@ -1,3 +1,20 @@
+# Copyright (C) 2019, 2020 Serghei Iakovlev <egrep@protonmail.ch>
+#
+# License
+#
+# This file is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 3
+# of the License, or (at your option) any later version.
+#
+# This file is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this file.  If not, see <https://www.gnu.org/licenses/>.
+
 import os
 import sys
 import pathlib
@@ -28,9 +45,48 @@ copyright_elisp = '''
 ;; along with this file.  If not, see <https://www.gnu.org/licenses/>.
 '''
 
-patterns = {'el': copyright_elisp}
-include_dirs = ('.', 'bin', 'share', 'site-lisp', 'test',)
-exlude_files = ('.dir-locals.el', 'package-quickstart.el', 'show-point-mode.el')
+copyright_generic = '''
+# Copyright (C) 2019, 2020 Serghei Iakovlev <egrep@protonmail.ch>
+#
+# License
+#
+# This file is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 3
+# of the License, or (at your option) any later version.
+#
+# This file is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this file.  If not, see <https://www.gnu.org/licenses/>.
+'''
+
+patterns = {
+    'el': copyright_elisp,
+    'py': copyright_generic,
+    'sh': copyright_generic,
+    'yml': copyright_generic,
+    'wrapper': copyright_generic,
+}
+
+include_dirs = (
+    '.',
+    '.github',
+    'bin',
+    'share',
+    'site-lisp',
+    'test',
+)
+
+exlude_files = (
+    '.dir-locals.el',
+    'package-quickstart.el',
+    'show-point-mode.el'
+)
+
 bad_licenses = []
 
 for ext in patterns:
