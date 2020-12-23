ce36eb2d34db9d593c07dc1d4e4295b53a6932bf
Serghei Iakovlev <egrep@protonmail.ch>
2020-12-23 16:01:18 +0200

    Correct license format

--- test/check-licenses.py
+++ test/check-licenses.py
@@ -1,7 +1,5 @@
 # Copyright (C) 2019, 2020 Serghei Iakovlev <egrep@protonmail.ch>
 #
-# License
-#
 # This file is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 3
@@ -47,8 +45,6 @@ copyright_elisp = '''
 
 copyright_generic = '''
 # Copyright (C) 2019, 2020 Serghei Iakovlev <egrep@protonmail.ch>
-#
-# License
 #
 # This file is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
