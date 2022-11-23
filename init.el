e2ba1467efdf38aeee8ce4d269ae6ff25bbd5bd5
Serghei Iakovlev <egrep@protonmail.ch>
2022-11-23 10:36:21 +0100

    Add initial config prelude

---
+++ init.el
@@ -0,0 +1,36 @@
+;;; init.el --- Initialization file. -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2019, 2020, 2021, 2022 Serghei Iakovlev <egrep@protonmail.ch>
+
+;; Author: Serghei Iakovlev <egrep@protonmail.ch>
+;; URL: https://github.com/sergeyklay/.emacs.d
+
+;; This file is NOT part of Emacs.
+
+;;;; License
+
+;; This file is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License
+;; as published by the Free Software Foundation; either version 3
+;; of the License, or (at your option) any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this file.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;   This file is used to initialize GNU Emacs for my daily needs.
+;; I started this project on 4 March 2019 from this commit:
+;; eb11ce25b0866508e023db4b8be6cca536cd3044
+
+;;; Code:
+
+;; Begin initialization
+(require 'prelude (concat user-emacs-directory "site-lisp/prelude"))
+
+;;; init.el ends here
