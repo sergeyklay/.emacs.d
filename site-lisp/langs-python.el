5612936950e71f5aae377a4f41a7fddc5766c35a
Serghei Iakovlev <egrep@protonmail.ch>
2022-12-03 16:37:58 +0100

    Setup python environamen

--- site-lisp/langs-python.el
+++ site-lisp/langs-python.el
@@ -34,31 +34,74 @@
 ;;; Code:
 
 (eval-when-compile
+  (require 'projects)
   (require 'directories))
+
+(require 'f)
+
+;; Silence linter
+(declare-function
+ projectile-project-name
+ "projectile" (&optional PROJECT))
 
 (use-package python
   :ensure nil
   :after company
   :custom
-  (python-shell-interpreter-args "-i --simple-prompt --pprint")
   (python-shell-interpreter "python3")
+  (python-shell-interpreter-args "-i --simple-prompt --pprint")
   :hook (python-mode . company-mode))
 
-(use-package python-environment
-  :defer t
-  :custom
-  (python-environment-directory (concat user-local-dir ".python-environments"))
-  (python-environment-virtualenv
-   `("virtualenv" "--system-site-packages" "--python"
-     ,(executable-find "python3"))))
+(defun my/pyenv-activate-current-project ()
+  "Activate pyenv version if .python-version file exists."
+  (interactive)
+  (f-traverse-upwards
+   (lambda (path)
+     (let ((pyenv-version-path (f-expand ".python-version" path))
+           pyenv-current-version)
+       (when (f-exists? pyenv-version-path)
+           (progn
+             (setq pyenv-current-version
+                   (s-trim (f-read-text pyenv-version-path 'utf-8)))
+             (pyenv-mode-set pyenv-current-version)
+             (pyvenv-workon pyenv-current-version)))))))
 
-;; Tidy up (require python package 'autopep8').
-(use-package py-autopep8
-  :defer t
+;; I use pyenv on a daily basis.
+(use-package pyenv-mode
+  :if (executable-find "pyenv")
+  :after projectile
+  :init
+  (setenv "WORKON_HOME" "~/.pyenv/versions/")
   :custom
-  (py-autopep8-options
-   '("--max-line-length=80"))
-  :hook (python-mode . py-autopep8-enable-on-save))
+  ;; pyenv global 3.11.0
+  (flycheck-python-pycompile-executable
+   (expand-file-name "~/.pyenv/shims/python"))
+  ;; pip install flake8
+  (flycheck-python-flake8-executable
+   (expand-file-name "~/.pyenv/shims/flake8"))
+  ;; pip install mypy
+  (flycheck-python-mypy-executable
+   (expand-file-name "~/.pyenv/shims/mypy"))
+  :config
+  ;; I set `exec-path' from `$PATH' variable. Therefore,
+  ;; there is no need to set this variable here.
+  ;;
+  ;; (add-to-list 'exec-path "~/.pyenv/shims")
+  ;;
+  (pyenv-mode)
+  (defun my|projectile-pyenv-mode-hook ()
+    "Set pyenv version matching project name."
+    (let ((project (projectile-project-name)))
+      (if (and (fboundp 'pyenv-mode-versions)
+               (member project (pyenv-mode-versions)))
+          (pyenv-mode-set project)
+        (pyenv-mode-unset))))
+  :bind
+  ("C-c p e" . my/pyenv-activate-current-project)
+  :hook
+  ((after-init .pyenv-init)
+   (python-mode . pyenv-mode)
+   (projectile-switch-project . my|projectile-pyenv-mode-hook)))
 
 (provide 'langs-python)
 ;;; langs-python.el ends here
