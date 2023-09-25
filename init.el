b47557282c0e8d89c79a8f8ded7ab4a9032c555d
Serghei Iakovlev <egrep@protonmail.ch>
2023-09-25 13:24:02 +0200

    Add initial project information

---
+++ init.el
@@ -0,0 +1,35 @@
+;;; init.el --- Initialization file. -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2019-2023 Serghei Iakovlev <egrep@protonmail.ch>
+
+;; Author: Serghei Iakovlev <egrep@protonmail.ch>
+;; URL: https://github.com/sergeyklay/.emacs.d
+
+;; This file is NOT part of Emacs.
+
+;;;; License
+
+;; This file is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License
+;; as published by the Free Software Foundation; either version 3
+;; of the License, or (at your option) any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this file.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;   This file bootstraps the configuration, which is divided into a number of
+;; other files.
+;;
+;; I started this project on 4 March 2019 from this commit:
+;; eb11ce25b0866508e023db4b8be6cca536cd3044
+
+;;; Code:
+					;
+;;; init.el ends here
