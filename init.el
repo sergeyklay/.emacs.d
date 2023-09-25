21f84d34f1ce074991820d189cf3263547743529
Serghei Iakovlev <egrep@protonmail.ch>
2023-09-25 17:45:37 +0200

    Move custom variables to the common place

--- init.el
+++ init.el
@@ -31,5 +31,20 @@
 ;; eb11ce25b0866508e023db4b8be6cca536cd3044
 
 ;;; Code:
-					;
+
+;;;; Custom variables
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(m4-program "/opt/homebrew/opt/m4/bin/m4"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(font-lock-string-face ((t (:foreground "OrangeRed")))))
+
 ;;; init.el ends here
