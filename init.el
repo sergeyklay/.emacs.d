9a621bb4f586428a8fdbd95e3991c645a7312c49
Serghei Iakovlev <egrep@protonmail.ch>
2023-09-27 00:04:47 +0200

    Switch to leuven theme

--- init.el
+++ init.el
@@ -34,11 +34,11 @@
 
 ;;;; Custom variables
 
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(font-lock-string-face ((t (:foreground "OrangeRed")))))
+ '(custom-enabled-themes '(leuven)))
 
 ;;; init.el ends here
