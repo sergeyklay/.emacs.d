bc0402f83252e5e49c65861a0647b1ccf6b4e54a
Serghei Iakovlev <egrep@protonmail.ch>
2023-09-27 23:19:00 +0200

    Add early-init.el file

---
+++ early-init.el
@@ -0,0 +1,78 @@
+;; early-init.el --- Early init file to use for Emacs >= 27.x -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2019-2023 Serghei Iakovlev <egrep@protonmail.ch>
+
+;; Author: Serghei Iakovlev <egrep@protonmail.ch>
+;; URL: https://github.com/sergeyklay/.emacs.d
+
+;; This file is NOT part of Emacs.
+
+;;;; License
+
+;; This file is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License
+;; as published by the Free Software Foundation; either version 3
+;; of the License, or (at your option) any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this file.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Deal with packages only and early init stuff.
+
+;;; Code:
+
+(when (or (daemonp)
+          noninteractive)
+  (setq package-enable-at-startup nil))
+
+;; Increasing GC is a common way to speed up Emacs. `gc-cons-threshold'
+;; sets at what point Emacs should invoke its garbage collector.  When
+;; set it temporarily to a large number, we only garbage collect once
+;; on startup.  We'll reset it later by enabling `gcmh-mode'. Not
+;; resetting it will cause stuttering/freezes.
+(setq gc-cons-threshold most-positive-fixnum
+      gc-cons-percentage 0.6)
+
+;; The command-line option ‘-batch’ causes Emacs to run noninteractively.
+;; In noninteractive sessions, prioritize non-byte-compiled source files to
+;; prevent the use of stale byte-code. Otherwise, it saves us a little IO time
+;; to skip the mtime checks on every *.elc file.
+(setq load-prefer-newer noninteractive)
+
+;; Contrary to what many Emacs users have in their configs, you don't need
+;; more than this to make UTF-8 the default coding system:
+(set-language-environment "UTF-8")
+
+;; `set-language-enviornment' sets `default-input-method', which is unwanted.
+(setq default-input-method nil)
+
+;; Prevent the glimpse of un-styled Emacs by disabling these UI elements early.
+(setq tool-bar-mode nil)
+(when (fboundp 'set-scroll-bar-mode)
+  (set-scroll-bar-mode nil))
+
+;; Currently I use menubar on graphical mode.
+(when (and (not (display-graphic-p)) (fboundp 'menu-bar-mode))
+  (menu-bar-mode -1))
+
+;; Resizing the Emacs frame can be a terribly expensive part of changing the
+;; font. By inhibiting this, we easily halve startup times with fonts that are
+;; larger than the system default.
+(setq frame-inhibit-implied-resize t)
+
+;; Remove command line options that aren't relevant to our current OS; means
+;; slightly less to process at startup.
+(unless (eq system-type 'darwin)
+  (setq command-line-ns-option-alist nil))
+(unless (eq system-type 'gnu/linux)
+  (setq command-line-x-option-alist nil))
+
+(provide 'early-init)
+;;; early-init.el ends here
