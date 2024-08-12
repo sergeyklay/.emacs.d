1c2dbfa239ae9a55bebc2a8470466fdd6199a737
Serghei Iakovlev <egrep@protonmail.ch>
2024-08-12 21:44:51 +0200

    Added ivy, smex, counsel and swiper

--- init.el
+++ init.el
@@ -387,6 +387,110 @@ buffers related to your current project."
   :commands (magit magit-status)
   :bind (("C-x g" . magit-status)))
 
+;;;; Setup completion
+
+(use-package ivy
+  :ensure t
+  :defer 0.1
+  :custom
+  ;; Don't show "./" and "../"
+  (ivy-extra-directories '())
+  ;; Show candidate index and total count
+  (ivy-count-format "(%d/%d) ")
+  ;; Number of lines for the minibuffer window
+  (ivy-height 12)
+  ;; do not set `completion-in-region-function'
+  (ivy-do-completion-in-region nil)
+  ;; Wrap around after the first and the last candidate
+  (ivy-wrap t)
+  ;; Fix the height of the minibuffer during ivy completion
+  (ivy-fixed-height-minibuffer t)
+  ;; Add recent files and bookmarks to `ivy-switch-buffer'
+  (ivy-use-virtual-buffers t)
+  ;; Don't use ^ as initial input
+  (ivy-initial-inputs-alist nil)
+  ;; Disable magic slash on non-match
+  (ivy-magic-slash-non-match-action nil)
+  (ivy-ignore-buffers '("\\` " "\\`\\*tramp/" "\\*Messages\\*" "TAGS"))
+  ;; Allow minibuffer commands while in the minibuffer
+  (enable-recursive-minibuffers t)
+  :bind (([remap switch-to-buffer] . ivy-switch-buffer)
+         ("C-x B"                  . ivy-switch-buffer-other-window)
+         ("C-c C-r"                . ivy-resume)
+         :map ivy-minibuffer-map
+         ("<tab>"                  . ivy-alt-done)
+         ("C-i"                    . ivy-partial-or-done)
+         :map ivy-switch-buffer-map
+         ("C-k"                    . ivy-switch-buffer-kill))
+  :config
+  (ivy-mode 1)
+  (when (fboundp 'ivy-format-function-line)
+    ;; Highlight til EOL
+    (setcdr (assq t ivy-format-functions-alist) #'ivy-format-function-line)))
+
+;; With smex installed, `counsel-M-x' lists all available commands but places
+;; the most recently used ones on top of the list ordered by frequency.
+(use-package smex
+  :ensure t
+  :commands smex
+  :custom
+  (smex-history-length 20)
+  :config
+  (smex-initialize))
+
+(use-package counsel
+  :ensure t
+  :hook (ivy-mode . counsel-mode)
+  :custom
+  (counsel-find-file-ignore-regexp
+   (concat
+    ;; file names beginning with #
+    "\\(?:^[#]\\)"
+    ;; file names ending with # or ~
+    "\\|\\(?:[#~]$\\)"
+    ;; file names beginning with Icon
+    "\\|\\(?:^Icon?\\)"
+    ;; macOs' custom attributes of its containing folder
+    "\\|\\(?:^\\.DS_Store$\\)"
+    ;; zsh compiled functions
+    "\\|\\(?:\\.zwc$\\)"
+    ;; Python bytecode
+    "\\|\\(?:^__pycache__/?$\\)"))
+  ;; Let counsel-find-file-at-point choose the file under cursor
+  (counsel-find-file-at-point t)
+  (counsel-rg-base-command
+   `("rg"
+     "--max-columns" "240" ; Don't print the lines longer than 240 bytes
+     "--with-filename"     ; Display the file path for matches
+     "--no-heading"        ; Don't group matches by each file
+     "--line-number"       ; Show line numbers (1-based)
+     "--color" "never"     ; Colors  will never be used
+     "--hidden"            ; Search hidden files and directories
+     "%s"))
+  :bind (("C-x C-i"                   . counsel-imenu)
+         ("C-x l"                     . counsel-locate)
+         ("C-h u"                     . counsel-unicode-char)
+         ("C-h C-l"                   . counsel-find-library)
+         ("C-h C-o"                   . counsel-info-lookup-symbol)
+         ("C-c k"                     . counsel-rg)
+         ("C-c f"                     . counsel-recentf)
+         ("C-c C-h"                   . counsel-command-history)
+         ([remap list-buffers]        . counsel-ibuffer)
+         ([remap eshell-list-history] . counsel-esh-history)
+         ([remap describe-symbol]     . counsel-describe-symbol)
+         :map ivy-minibuffer-map
+         ("C-r"                       . counsel-minibuffer-history)
+         :map minibuffer-local-map
+         ("C-r"                       . counsel-minibuffer-history)))
+
+(use-package swiper
+  :ensure t
+  :after ivy
+  :commands (swiper swiper-all)
+  :bind(([remap isearch-forward] . swiper)
+        :map swiper-map
+        ("M-%" . swiper-query-replace)))
+
 ;;;; IRC and other communication
 (use-package erc
   :after auth-source
